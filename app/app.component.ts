aa86a83cf8218debf3683269e265a7c926107d2d
Kameron <khayes@hotohuseinc.com>
2018-08-30 17:32:01 -0400

    create passenger interface and use ngfor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,48 @@
 import { Component } from '@angular/core';
 
+interface Passenger{
+  id: number,
+  fullname: string,
+  checkedIn: boolean,
+  checkInDate?: number
+
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-    <input
-      tpye="text"
-      [value]="name"
-      (input)="handleChange($event.target.value)">
-      <div *ngIf="name.length">
-        Searching for...{{name}}
-      </div>
+      <h3>{{title}}</h3>
+      <ul>
+      <li *ngFor="let passenger of passengers; let i =index;">
+      <span
+        class="status"
+        [class.checked-in]="passenger.checkedIn"></span>
+        {{i}}: {{passenger.fullname}}
+        <p>{{passenger | json}}</p><!--Json Pipe-->
+      </li>
+      </ul>
     </div>
   `
 })
 export class AppComponent {
   title: string;
-  name: string = '';
-  handleChange(value: string){
-    this.name = value;
-  }
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Kam',
+    checkedIn: true,
+    checkInDate: 7181995
+    },
+    {
+    id: 2,
+    fullname: 'Kam2',
+    checkedIn: false,
+    checkInDate: 201823
+  }];
 
   constructor(){
-    this.title = 'Ultimate Angular';
+    this.title = 'Airline Passengers';
   }
 
 }
