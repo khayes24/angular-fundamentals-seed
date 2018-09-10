72bc15a9312b5efd10e54215ef7eed6af033a02d
Kameron <khayes@hotohuseinc.com>
2018-09-10 09:31:37 -0400

    add components to passenger dashboard container

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'passenger-count',
+  template:  `
+    <div>
+      Count Component!
+    </div>
+  `
+})
+
+export class PassengerCountComponent{
+  constructor(){
+
+  }
+}
---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'passenger-detail',
+  template:  `
+    <div>
+      Detail Component!
+    </div>
+  `
+})
+
+export class PassengerDetailComponent{
+  constructor(){
+
+  }
+}
