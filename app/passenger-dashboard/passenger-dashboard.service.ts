3c0cba39ae33f7401273ae39b780202c7092593f
Kameron <khayes@hotohuseinc.com>
2018-09-21 16:32:04 -0400

    create passenger view component and import it into app

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -32,6 +32,13 @@ Observable provides operators like map, forEach, reduce,... similar to an array
     .catch((error: any) => Observable.throw(error.json()));
   }
 
+  getPassenger(id: number): Observable<Passenger>{
+    return this.http
+    .get(`${PASSENGER_API}/${id}`)
+    .map((response: Response) => response.json())
+    .catch((error: any) => Observable.throw(error.json()));
+  }
+
   updatePassenger(passenger: Passenger): Observable<Passenger>{
     return this.http
     .put(`${PASSENGER_API}/${passenger.id}`, passenger)
