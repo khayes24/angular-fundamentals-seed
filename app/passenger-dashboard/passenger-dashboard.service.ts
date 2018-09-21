6f2a11e03b0f3da1b175682ec2eadb77716cd873
Kameron <khayes@hotohuseinc.com>
2018-09-21 16:19:20 -0400

    add throw/catch modules

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,10 +1,13 @@
 
 import { Injectable } from '@angular/core';
 
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+//Import throw and catch modules
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
 
 import { Passenger } from './models/passenger.interface';
 
@@ -14,22 +17,32 @@ const PASSENGER_API: string = '/api/passengers';
 @Injectable()
 export class PassengerDashboardService{
   constructor(private http: Http) {}
+/*
+What is an observable?
+An Observable is like a Stream (in many languages) and allows to pass zero or
+more events where the callback is called for each event.
 
+Observable provides operators like map, forEach, reduce,... similar to an array
+
+*/
   getPassengers(): Observable<Passenger[]>{
     return this.http
     .get(PASSENGER_API)
-    .map((response: Response) => response.json());
+    .map((response: Response) => response.json())
+    .catch((error: any) => Observable.throw(error.json()));
   }
 
   updatePassenger(passenger: Passenger): Observable<Passenger>{
     return this.http
     .put(`${PASSENGER_API}/${passenger.id}`, passenger)
-    .map((response: Response) => response.json());
+    .map((response: Response) => response.json())
+    .catch((error: any) => Observable.throw(error.json()));
   }
 
   removePassenger(passenger: Passenger): Observable<Passenger>{
     return this.http
     .delete(`${PASSENGER_API}/${passenger.id}`)
-    .map((response: Response) => response.json());
+    .map((response: Response) => response.json())
+    .catch((error: any) => Observable.throw(error.json()));
   }
 }
