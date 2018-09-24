76322feb8c0fb274dbb0b5e8d419167bfc326382
Kameron <khayes@hotohuseinc.com>
2018-09-24 17:44:34 -0400

    ng form components

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,23 +1,111 @@
 import { Component, Input } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
 
 @Component({
   selector: 'passenger-form',
   styleUrls: ['passenger-form.component.scss'],
   template: `
-    <form>
-      Form!
+    <form #form="ngForm" novalidate>
+      {{detail | json}}
       <div>
-        {{detail | json}}
+        Passenger name:
+        <input
+          type="text"
+          name="fullname"
+          [ngModel]="detail?.fullname">
       </div>
+
+      <div>
+        Passenger ID:
+        <input
+          type="number"
+          name="id"
+          [ngModel]="detail?.id">
+      </div>
+
+      <div>
+        <label>
+          <input
+            type="radio"
+            [value]="true"
+            name="checkedIn"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+            Yes
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            [value]="false"
+            name="checkedIn"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+            No
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            name="checkedIn"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+
+        </label>
+      </div>
+
+      <div *ngIf="form.value.checkedIn">
+        Check in Date
+        <input
+          type="number"
+          name="checkInDate"
+          [ngModel]="detail?.checkInDate">
+      </div>
+
+      <div>
+        Luggage
+        <select
+          name="baggage"
+          [ngModel]="detail?.baggage">
+          <option
+            *ngFor="let item of baggage">
+          </option>
+        </select>
+      </div>
+      {{ form.value | json }}
     </form>
   `
 })
 
 export class PassengerFormComponent {
+
   @Input()
   detail: Passenger;
+
+  baggage:Baggage[] = [{
+    key: 'none',
+    value: 'no baggage'
+  },
+  {
+    key: 'hand-only',
+    value: 'Hand baggage'
+  },
+  {
+    key: 'hold-only',
+    value: 'hold baggage'
+  },
+  {
+    key: 'hold-hand',
+    value: 'hold & hand baggage'
+  }];
+
+  toggleCheckIn(checkedIn: boolean){
+    if(checkedIn){
+      this.detail.checkInDate = Date.now();
+    }
+  }
 
   constructor() {}
 }
