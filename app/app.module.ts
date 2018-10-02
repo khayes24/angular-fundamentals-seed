a668eb2e476d2f1c23b1a904af9292a9fd50612f
Kameron <khayes@hotohuseinc.com>
2018-10-01 21:48:45 -0400

    create home component using router module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent , pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     //Custom Modules
     PassengerDashboardModule
   ],
@@ -16,7 +23,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ]
 })
 export class AppModule {}
